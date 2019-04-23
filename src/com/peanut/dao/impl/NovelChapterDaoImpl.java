fc8330b5d1191469c4d5fbee74fefa658817808d
LeonTheProfessional <leonthekiller@foxmail.com>
2019-04-23 16:00:56 +0800

    增加了novel dao接口和dao实现类。

---
+++ src/com/peanut/dao/impl/NovelChapterDaoImpl.java
@@ -0,0 +1,15 @@
+package com.peanut.dao.impl;
+
+import com.peanut.dao.NovelChapterDao;
+import com.peanut.entity.pojo.novel.NovelChapter;
+
+/**
+ * description: 小说章节接口实现类.
+ *
+ * @author Doc10th
+ * @date 2019-04-23
+ * @see com.peanut.dao.impl
+ * @since 1.0
+ */
+public class NovelChapterDaoImpl extends BaseDaoImpl<NovelChapter> implements NovelChapterDao {
+}
